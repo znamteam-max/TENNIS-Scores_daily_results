7e8ba7c22a9e807321d3a6e2dcb6c5bc5e044b48
znamteam-max <znamteam@gmail.com>
2025-11-14 12:04:19 +0300

    Update hello.py

--- api/hello.py
+++ api/hello.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# ловим оба варианта путей — со срезом и без
 @app.get("")
 @app.get("/")
+@app.get("/api/hello")
+@app.get("/api/hello/")
 def hi():
     return {"ok": True}
